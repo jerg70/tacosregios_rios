5c9eb4155ae15027141145d5000524abbc1e02cb
JERG <jerg70@gmail.com>
2022-08-31 20:58:59 -0500

    Correcion de menu hamburguesa, link a script de bootstrap faltante

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -17,9 +17,9 @@ const NavBar = () => {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item"><a className="nav-link" href="../../public/index.html">Inicio</a></li>
-              <li className="nav-item"><a className="nav-link" href="">Nosotros</a></li>
-              <li className="nav-item"><a className="nav-link" href="">Menu</a></li>
-              <li className="nav-item"><a className="nav-link" href="">Contacto</a></li>
+              <li className="nav-item"><a className="nav-link" href="#">Nosotros</a></li>
+              <li className="nav-item"><a className="nav-link" href="#">Menu</a></li>
+              <li className="nav-item"><a className="nav-link" href="#">Contacto</a></li>
             </ul>
           </div>
           <CartWidget />
